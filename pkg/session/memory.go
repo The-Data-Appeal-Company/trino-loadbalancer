af8f2791c379c576ec73b3a23855f1e216830c22
GaruGaru <garuglieritommaso.dev@gmail.com>
2020-10-10 15:07:40 +0200

    thread safe memory session storage

--- pkg/session/memory.go
+++ pkg/session/memory.go
@@ -4,31 +4,43 @@ import (
 	"context"
 	"fmt"
 	"github.com/The-Data-Appeal-Company/presto-loadbalancer/pkg/models"
+	"sync"
 )
 
 type Memory struct {
 	sessions map[string]string
+	mutex    *sync.RWMutex
 }
 
 func NewMemoryStorage() *Memory {
 	return &Memory{
 		sessions: make(map[string]string),
+		mutex:    &sync.RWMutex{},
 	}
 }
 
 func (m *Memory) Link(ctx context.Context, info models.QueryInfo, s string) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
 	hash := m.queryHash(info)
 	m.sessions[hash] = s
 	return nil
 }
 
 func (m *Memory) Unlink(ctx context.Context, info models.QueryInfo) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
 	hash := m.queryHash(info)
 	delete(m.sessions, hash)
 	return nil
 }
 
 func (m *Memory) Get(ctx context.Context, info models.QueryInfo) (string, error) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
 	hash := m.queryHash(info)
 	val, present := m.sessions[hash]
 
