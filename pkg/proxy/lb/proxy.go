1654ea963b6c6dcc72c2b3e8c497ead4ae9a913e
garugaru <garuglieritommaso.dev@gmail.com>
2023-11-06 21:18:39 +0100

    wrap ErrRoutingNotFound using ErrNoBackendsAvailable

--- pkg/proxy/lb/proxy.go
+++ pkg/proxy/lb/proxy.go
@@ -112,6 +112,9 @@ func (p *Proxy) selectCoordinatorForRequest(request *http.Request) (CoordinatorR
 
 		targetCoordinator, err := p.router.Route(routingRequest(healthyCoordinators, request))
 		if err != nil {
+			if errors.Is(err, routing.ErrRoutingNotFound) {
+				return CoordinatorRef{}, fmt.Errorf("%w: %w", err, ErrNoBackendsAvailable)
+			}
 			return CoordinatorRef{}, err
 		}
 
